e625ba77dd1412267833d29f3e01a816e2348f04
Dawit Woldegiorgis <dawithw@gmail.com>
2020-05-18 11:17:17 -0500

    clean start

--- script.js
+++
@@ -1,6 +0,0 @@
-document.onload = function () {
-  var button = document.getElementById("button");
-  button.onclick = function(){
-    document.getElementById("content").innerText = "Button is clicked!";
-  };
-};
