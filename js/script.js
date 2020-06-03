80f406c0022e45d7e7ed9d6ef2c1a9e07b740fc8
Dawit Woldegiorgis <dawithw@gmail.com>
2020-06-02 19:15:27 -0500

    about-me and skeleton of projects; edited images

--- js/script.js
+++ js/script.js
@@ -1,6 +1,35 @@
 $(document).ready(function() {
-    $(".menu > .icon").click(function(event){
-        $(".menu ul").toggleClass("hide-mobile");
-        console.log($(".menu ul"));
-    });
+
+    /* toggle mobile menu */
+    function toggleMenu(){
+        $(".main-menu .links").slideToggle(200);
+        $(".hamburger-icon").toggleClass("toggled");
+        $(".main-logo").toggleClass("toggled");
+        console.log("clicked");
+    }
+
+    function toggleDetails(event) {
+        $(event.currentTarget).css({
+                    'transform' : 'rotate(180deg)',
+                    'WebkitTransform' : 'rotate(180deg)',
+                    '-moz-transform' : 'rotate(180deg'});
+        var arrowId = $(event.currentTarget).attr('id');
+        // id has format "detail-arrow-{num}"
+        var arrowNumber = arrowId.split('-')[2];
+        $("#detail-" + arrowNumber).toggleClass("hide");
+    }
+
+    $(".hamburger-icon").click(toggleMenu);
+    $(".nav-list li").click(toggleMenu);
+
+    $(".arrow").click(toggleDetails);
+
+    // var pages = ["home", "about-me", "projects", "contact"];
+    // var active = 0;
+    // $(document).scroll(() => {
+    //     console.log($("." + pages[active]))
+    //     console.log($($(".main-container").position()));
+    // })
+    
 });
+
