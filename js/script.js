09fc884d01057abffd5a6447257d752d9cff1d3b
Dawit Woldegiorgis <dawithw@gmail.com>
2020-06-22 19:07:53 -0500

    completed UI

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
 
-    
     /* toggle mobile menu */
     function toggleMenu(){
-        if(document.documentElement.clientWidth < 750) {
+        if(document.documentElement.clientWidth < 768) {
             $(".page-links").slideToggle(200);
         }
     }
@@ -12,24 +11,67 @@ $(document).ready(function() {
     $(".hamburger-icon").click(toggleMenu);
     $(".nav-list li").click(toggleMenu);
 
-    /* toggle highlight css for children text */
-    function highlight() {
-        $($(this).children()[0]).toggleClass("highlight");
-    };
-
-    /* highlight social media links on hover */
-    $(".social-media svg").hover(highlight);
 
     /* animated scroll */
     function animatedScroll(event) {
         event.preventDefault();
         $("html, body").animate({ 
-            scrollTop: $($(this).attr('href') ).offset().top - 73
+            scrollTop: $($(this).attr('href') ).offset().top - 60
         }, 750);
     }
 
+    /* animated scroll on click for page navigation links*/
     $(".nav-list a").click(animatedScroll);
     $(".main-logo").click(animatedScroll);
+
+
+    /* toggle highlight css for children text */
+    function fill(svgClass, color) {
+        svgClass = "." + svgClass;
+        $(svgClass + " g").css({"stroke" : color});
+        $(svgClass + " path").css({"fill" : color});
+    };
+
+    /* fill svg child to white */
+    function highlight() {
+        let elemClass = $(this).attr("class");
+        fill(elemClass, "#fff");
+    }
+
+    /* fill svg child to original color */
+    function restore() {
+        // should be same as ".social-media path" fill defined in the style.css file
+        let originalColor =  "#bbb";        
+        let elemClass = $(this).attr("class");
+        fill(elemClass, originalColor);
+    }
+
+    /* highlight social media links on hover or touch */
+    $(".linkedin, .github, .email").on({
+        "mouseenter" : highlight,
+        "mouseleave" : restore,
+        "touchstart" : highlight,
+        "touchend" : restore
+    });
+
+    /* filling and unfilling toggled buttons */
+
+    function fillButton() {
+        $(this).addClass('button-fill');
+    }
+
+    function unfillButton() {
+        $(this).removeClass('button-fill');
+    }
+
+    $(".button, #projects article a").on({
+        "mouseenter": fillButton,
+        "mouseleave": unfillButton,
+        "touchstart" : fillButton, 
+        "touchend" : unfillButton}
+    );
+
+    /* --- end button fill ---- */
 
 });
 
